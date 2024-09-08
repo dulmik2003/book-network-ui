e4c351a9eb0aa569dc76258143d874f1c2a089d3
Dulmika <150633665+dulmik2003@users.noreply.github.com>
2024-09-08 17:05:44 +0530

    create user registration page

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RegisterRequest } from '../../services/models';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
+import { ApiModule } from '../../services/api.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, NgIf, NgFor ,ApiModule, HttpClientModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+  registerRequest: RegisterRequest = {email: '', firstName: '', lastName: '', password: ''};
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+    // console.log('RegisterComponent loaded...');
+  }
+
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+    this.errorMsg = [];
+    this.authService.register({
+      body: this.registerRequest
+    })
+      .subscribe({
+        next: () => {
+          this.router.navigate(['activate-account']);
+        },
+        error: (err) => {
+          console.log(err.error);
+          
+          this.errorMsg = err.error.validationErrors;
+        }
+      });
+  }
+}
